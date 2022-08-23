e13af6f36d3c01e0aa0eec7081a1f87bc49e55ad
Cameron Fleming <cameron@nevexo.space>
2022-08-23 12:23:32 +0100

    Add sketchy script for AcePC

---
+++ update_and_start.ps1
@@ -0,0 +1,13 @@
+Write-Host "Fetching Git Tree..."
+git pull 
+Write-Host "Start Servers..."
+$pwd = Get-Location
+$PORT = "COM3"
+
+$env:PORT_OVERRIDE=$PORT
+Start-Process -WindowStyle Maximized powershell -ArgumentList '-noexit -command "Set-Location $pwd; Set-Location server; node index.js"'
+Start-Process -WindowStyle Maximized powershell -ArgumentList '-noexit -command "Set-Location $pwd; Set-Location com-scanner-agent; node index.js"'
+Start-Process -WindowStyle Maximized powershell -ArgumentList '-noexit -command "Set-Location $pwd; Set-Location frontend; py -m http.server 80"'
+
+Write-Host "Launch browser"
+Start-Process -WindowStyle Maximized powershell -ArgumentList '-noexit -command "C:\Program` Files\Google\Chrome\Application\chrome.exe http://localhost --kiosk"'
