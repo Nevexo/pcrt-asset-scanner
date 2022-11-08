6232a2464d1822b0fe4e993fb54a163abdccdbae
Cameron Fleming <cameron@nevexo.space>
2022-11-08 14:40:46 +0000

    Handle scanner agent correct

--- com-scanner-agent/index.js
+++ com-scanner-agent/index.js
@@ -145,7 +145,7 @@ const main = async () => {
 
     // The port has recieved data, send it to the server.
     port.on('data', async (data) => {
-        const string = data.toString.trim();
+        const string = data.toString().trim();
         if (string.length > process.env.DATA_LENGTH_LIMIT || 5) {
             logger.warn("Invalid item scanned, will not forward to server.");
             return;
